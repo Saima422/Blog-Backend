a30cf51e89529ca4c712eefe71b1b1c0fcc68de8
saima <sayedssaima04@gmail.com>
2021-09-02 03:29:17 +0530

    made some changes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
 
 app.use('/blogs', blogRouter);
 app.get('/random', getRandomBlog);
